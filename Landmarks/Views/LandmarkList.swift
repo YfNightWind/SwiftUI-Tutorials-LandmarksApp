43e2c916da94aca24810cf1badf1869057412052
YfNightWind <lyh1147557099@outlook.com>
2021-08-16 23:16:10 +0800

    reach section 4

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,19 +8,31 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         //只有包裹在了NavigationView才可以有navigationTitle
         NavigationView {
             //因为在Landmark.swift中说明了Identifiable
             //所以删除括号中的id: \.id
-            List (landmarks){ landmark in
-                
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly, label: {
+                    Text("Favorites only")
+                })
+                ForEach (filteredLandmarks){ landmark in
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
-            
         }
         
     }
